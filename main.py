616d0045ae86db93d1ce405441b871294981636a
graceapanos <gap4300@gmail.com>
2024-01-03 17:37:19 -0600

    langchain simple chatbot

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Hello
+import langchain
+import openai 
+import os 
+print("Hello World") 
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, AIMessage
+
+chat = ChatOpenAI()
+chat.stream()
+
+
